c6f5f82407d05b73addf640915499553da7a0c5b
adminlocal <papemor94@gmail.com>
2020-03-26 18:31:42 +0100

    second commmit

--- src/main/java/fr/univtln/login/App.java
+++ src/main/java/fr/univtln/login/App.java
@@ -17,24 +17,24 @@ public class App
         java.sql.Connection dbConnect = null;
         java.sql.Statement dbStatement = null;
         //dbConnect = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcdemo?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
-        dbConnect = DriverManager.getConnection(" jdbc:postgresql://postgres1:5432/mydb?user=postgres&password=mysecretpassword");
+        dbConnect = DriverManager.getConnection(" jdbc:postgresql://postgres1:5432/postgres?user=postgres&password=mysecretpassword");
         dbStatement = dbConnect.createStatement();
-        /*String sql = "CREATE TABLE REGISTRATION " +
+        String sql = "CREATE TABLE REGISTRATION " +
                 "(id INTEGER not NULL, " +
                 " first VARCHAR(255), " +
                 " last VARCHAR(255), " +
                 " age INTEGER, " +
                 " PRIMARY KEY ( id ))";
 
-        dbStatement.executeUpdate(sql);*/
-      /*  String sql2 = "INSERT INTO REGISTRATION VALUES (2, 'PapeMor', 'CISSE', 25)";
+        dbStatement.executeUpdate(sql);
+        String sql2 = "INSERT INTO REGISTRATION VALUES (2, 'PapeMor', 'CISSE', 25)";
         dbStatement.executeUpdate(sql2);
         ResultSet rs = dbStatement.executeQuery("SELECT * FROM REGISTRATION") ;
         if (rs != null) {
             while (rs.next()) {
                 System.out.println("Valeur: " + rs.getString(2));
             }
-        }*/
+        }
         System.out.println( "Hello World!" );
     }
 }
